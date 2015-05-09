0faebf7dbdac90a512a46224a551a1211efbbb82
bess <776536184@qq.com>
2015-05-09 10:48:44 +0800

    commit code
    Signed-off-by: bess <776536184@qq.com>

--- DacrsUI/SplashThread.cpp
+++ DacrsUI/SplashThread.cpp
@@ -21,22 +21,18 @@ CSplashThread::~CSplashThread()
 BOOL CSplashThread::InitInstance()
 {
 	// TODO: 在此执行任意逐线程初始化
-	cs_dlg.Lock();
 	m_pSplashDlg=new CStartProgress;   
 	//m_pSplashDlg->SetEnable(true);
 	m_pSplashDlg->Create(IDD_DIALOG_INIT);
 	m_pSplashDlg->ShowWindow(SW_SHOW);
-	cs_dlg.Unlock();
 	return TRUE;
 }
 
 int CSplashThread::ExitInstance()
 {
 	// TODO: 在此执行任意逐线程清理
-	cs_dlg.Lock();
 	m_pSplashDlg->DestroyWindow();
 	delete m_pSplashDlg;
-	cs_dlg.Unlock();
 	return CWinThread::ExitInstance();
 }
 
@@ -47,13 +43,5 @@ END_MESSAGE_MAP()
 // CSplashThread 消息处理程序
 void CSplashThread::HideSplash()
 {
-	cs_dlg.Lock();
 	m_pSplashDlg->SendMessage(WM_CLOSE);
-	cs_dlg.Unlock();
-}
-void CSplashThread::SetDlgPos(int pos)
-{
-	cs_dlg.Lock();
-	m_pSplashDlg->SetProgessRange(pos);
-	cs_dlg.Unlock();
 }
