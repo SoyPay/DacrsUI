0e29ae37df376d2a8b5f55c6767953db54b929a6
ranger.shi <ranger.shi@gmail.com>
2015-05-10 19:45:42 +0800

    add some  undo dlg
    Former-commit-id: ba97fb30ced7da356896562b782d8081b97d2827

--- DacrsUI/TradDlg.cpp
+++ DacrsUI/TradDlg.cpp
@@ -13,11 +13,15 @@ IMPLEMENT_DYNAMIC(CTradDlg, CDialogBar)
 
 CTradDlg::CTradDlg()
 {
-
+	m_pBmp = NULL ;
 }
 
 CTradDlg::~CTradDlg()
 {
+	if( NULL != m_pBmp ) {
+		DeleteObject(m_pBmp) ;
+		m_pBmp = NULL ;
+	}
 }
 
 void CTradDlg::DoDataExchange(CDataExchange* pDX)
@@ -27,7 +31,56 @@ void CTradDlg::DoDataExchange(CDataExchange* pDX)
 
 
 BEGIN_MESSAGE_MAP(CTradDlg, CDialogBar)
+	ON_WM_CREATE()
+	ON_WM_ERASEBKGND()
 END_MESSAGE_MAP()
 
 
 // CTradDlg 消息处理程序
+
+void CTradDlg::SetBkBmpNid( UINT nBitmapIn ) 
+{
+	if( NULL != m_pBmp ) {
+		::DeleteObject( m_pBmp ) ;
+		m_pBmp = NULL ;
+	}
+	m_pBmp	= NULL ;
+	HINSTANCE	hInstResource = NULL;	
+	hInstResource = AfxFindResourceHandle(MAKEINTRESOURCE(nBitmapIn), RT_BITMAP);
+	if( NULL != hInstResource ) {
+		m_pBmp = (HBITMAP)::LoadImage(hInstResource, MAKEINTRESOURCE(nBitmapIn), IMAGE_BITMAP, 0, 0, 0);
+	}
+}
+int CTradDlg::OnCreate(LPCREATESTRUCT lpCreateStruct)
+{
+	if (CDialogBar::OnCreate(lpCreateStruct) == -1)
+		return -1;
+
+	SetBkBmpNid(IDB_BITMAP_TRAD_BJ);
+	// TODO:  在此添加您专用的创建代码
+
+	return 0;
+}
+
+
+BOOL CTradDlg::OnEraseBkgnd(CDC* pDC)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	CRect   rect; 
+	GetClientRect(&rect); 
+
+	if(m_pBmp   !=   NULL) { 
+		BITMAP   bm; 
+		CDC   dcMem; 
+		::GetObject(m_pBmp,sizeof(BITMAP),   (LPVOID)&bm); 
+		dcMem.CreateCompatibleDC(NULL); 
+		HBITMAP     pOldBitmap   =(HBITMAP   )   dcMem.SelectObject(m_pBmp); 
+		pDC-> StretchBlt(rect.left,rect.top-1,rect.Width(),rect.Height(),   &dcMem,   0,   0,bm.bmWidth-1,bm.bmHeight-1,   SRCCOPY); 
+
+		dcMem.SelectObject(pOldBitmap);
+		dcMem.DeleteDC();
+	} else  
+		CWnd::OnEraseBkgnd(pDC); 
+
+	return TRUE;
+}
