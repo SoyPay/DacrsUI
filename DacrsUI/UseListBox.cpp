5e4eeca82eeb223de2169291c5363b91aa30397f
bess <776536184@qq.com>
2015-06-14 16:26:35 +0800

    添加函数修改红包池按钮字体颜色
    Signed-off-by: bess <776536184@qq.com>
    
    Former-commit-id: fa70f0e1e17810e1d886d71c906f30a9874e841d

--- DacrsUI/UseListBox.cpp
+++ DacrsUI/UseListBox.cpp
@@ -301,6 +301,22 @@ void CUseListBox::SetIndexInage(int iIndex , UINT nButImage  )
 		pData->pBut2->SetColor(CButtonST::BTNST_COLOR_BK_IN, RGB(255, 255, 255));
 	}
 }
+void    CUseListBox::SetotherIndexInage(int iIndex , UINT nButImage  )
+{
+	{
+		List_AppendData *pData = GetAppendDataInfo(iIndex) ;
+		if ( NULL != pData ) {
+			//pData->pBut2->SetBitmaps( nButImage , RGB(255, 255, 0) , nButImage , RGB(255, 255, 255) );
+			pData->pBut2->SetBitmaps( nButImage , RGB(255, 255, 0), nButImage , RGB(255, 255, 255) );
+			pData->pBut2->SetAlign(CButtonST::ST_ALIGN_OVERLAP);
+			pData->pBut2->SetFontEx(20 , _T("Î¢ÈíÑÅºÚ"));
+			pData->pBut2->SetColor(CButtonST::BTNST_COLOR_FG_OUT , RGB(200, 75, 60));
+			pData->pBut2->SetColor(CButtonST::BTNST_COLOR_FG_IN , RGB(255, 255, 255));
+			pData->pBut2->SetColor(CButtonST::BTNST_COLOR_FG_FOCUS, RGB(200, 75, 60));
+			pData->pBut2->SetColor(CButtonST::BTNST_COLOR_BK_IN, RGB(200, 75, 60));
+		}
+	}
+}
 void CUseListBox::SetIndexBackCol(int iIndex ,  int nline ,COLORREF   col)
 {
 	List_AppendData *pData = GetAppendDataInfo(iIndex) ;
