f2d654b5b016ab8b5f819810578a2f1727867ad2
bess <776536184@qq.com>
2015-06-15 16:19:29 +0800

    显示用listcontrol
    Signed-off-by: bess <776536184@qq.com>
    
    Former-commit-id: ff8c9060ef851d551cda61e9c626f227c7497456

--- DacrsUI/RedPacketList.cpp
+++ DacrsUI/RedPacketList.cpp
@@ -24,6 +24,7 @@ CRedPacketList::~CRedPacketList()
 void CRedPacketList::DoDataExchange(CDataExchange* pDX)
 {
 	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_LIST2, m_listCtrl);
 }
 
 
@@ -84,33 +85,53 @@ void CRedPacketList::ShowTxDetail(CString txhash)
 	std::vector<unsigned char> vTemp = CSoyPayHelp::getInstance()->ParseHex(nValue.GetString());
 	memcpy(&redPacket, &vTemp[0], sizeof(uistruct::RED_DATA));
 
-	CString showdata = _T(" 地址                            金额 \r\n\r\n");
-	for (int i =0;i <redPacket.dbdata.takennum;i++)
+	CString showdata = _T(""); //= _T(" 地址                            金额 \r\n\r\n");
+	m_listCtrl.DeleteAllItems();
+	 if (redPacket.dbdata.type == 3 && !redPacket.dbdata.fover)
 	{
-		uistruct::USER_INFO userinfo = redPacket.userinfo[i];
-		std::vector<unsigned char> vSendid;
-		vSendid.assign(userinfo.regid,userinfo.regid+sizeof(userinfo.regid));
-		string regid  =CSoyPayHelp::getInstance()->GetNotFullRegID(vSendid);
-		double money = (userinfo.amount*1.0)/COIN;
-		showdata.AppendFormat(_T(" %s                       %.8f\r\n\r\n"),regid.c_str(),money);
-
-	}
-
-	 if (redPacket.dbdata.type == 3)
-	{
-		if (!redPacket.dbdata.fover)
+		//showdata.Format(_T("接龙红包还未抢完"));
+		::MessageBox( this->GetSafeHwnd() ,_T("接龙红包还未抢完"), _T("提示") , MB_ICONINFORMATION ) ;
+	}else{
+		for (int i =0;i <redPacket.dbdata.takennum;i++)
 		{
+			int nSubIdx = 0;
+			uistruct::USER_INFO userinfo = redPacket.userinfo[i];
+			std::vector<unsigned char> vSendid;
+			vSendid.assign(userinfo.regid,userinfo.regid+sizeof(userinfo.regid));
+			string regid  =CSoyPayHelp::getInstance()->GetNotFullRegID(vSendid);
+			double money = (userinfo.amount*1.0)/COIN;
+			//showdata.AppendFormat(_T(" %s                       %.8f\r\n\r\n"),regid.c_str(),money);
+			showdata.Format(_T("%d"),i);
+			m_listCtrl.InsertItem(i, showdata);					//序号
 
-			showdata.Format(_T("接龙红包还未抢完"));
+			showdata.Format(_T("%s"),regid.c_str());
+			m_listCtrl.SetItemText(i , ++nSubIdx , showdata );  //
+
+			strShowData.Format(_T("%.8f"), money); 
+			m_listCtrl.SetItemText(i , ++nSubIdx , strShowData );  //交易hash
+
 		}
-	}
+	 }
 
-	GetDlgItem(IDC_EDIT_TXDETAIL)->SetWindowText(showdata);
+	//GetDlgItem(IDC_EDIT_TXDETAIL)->SetWindowText(showdata);
 }
 BOOL CRedPacketList::OnInitDialog()
 {
 	CDialogEx::OnInitDialog();
 
+	struct LISTCol {
+		CString		name ;
+		UINT		size ;
+	} listcol[3]  = {
+		{"序号",          70},
+		{"账户ID",          85},
+		{"金额" ,      150}
+	};
+
+	for( int i = 0 ; i < 3 ; i++  ) {
+		m_listCtrl.InsertColumn(i,listcol[i].name,LVCFMT_CENTER,listcol[i].size);
+	}
+	m_listCtrl.SetExtendedStyle(LVS_EX_FULLROWSELECT | LVS_EX_GRIDLINES | LVS_EX_HEADERDRAGDROP );// |LVS_SINGLESEL  );
 	// TODO:  在此添加额外的初始化
 	ShowTxDetail(theApp.m_strAddress);
 	return TRUE;  // return TRUE unless you set the focus to a control
--- DacrsUI/RedPacketList.h
+++ DacrsUI/RedPacketList.h
@@ -21,4 +21,5 @@ protected:
 public:
 	virtual BOOL OnInitDialog();
 	void ShowTxDetail(CString txhash);
+	CListCtrl   m_listCtrl;
 };
