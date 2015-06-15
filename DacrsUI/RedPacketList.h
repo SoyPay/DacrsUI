6c058a156d7c9e1bb62b368074ee536470964147
bess <776536184@qq.com>
2015-06-15 11:37:59 +0800

    1.添加显示抢红包列表
    Signed-off-by: bess <776536184@qq.com>

---
+++ DacrsUI/RedPacketList.h
@@ -0,0 +1,24 @@
+#pragma once
+
+
+// CRedPacketList 对话框
+
+class CRedPacketList : public CDialogEx
+{
+	DECLARE_DYNAMIC(CRedPacketList)
+
+public:
+	CRedPacketList(CWnd* pParent = NULL);   // 标准构造函数
+	virtual ~CRedPacketList();
+
+// 对话框数据
+	enum { IDD = IDD_DIALOG_REDPACKETLIST };
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+public:
+	virtual BOOL OnInitDialog();
+	void ShowTxDetail(CString txhash);
+};
