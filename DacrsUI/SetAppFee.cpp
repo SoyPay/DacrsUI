d0cee757bc1e4f373d32b3892828b215d41bdcfc
bess <776536184@qq.com>
2015-07-01 19:10:25 +0800

    1.添加了菜单设置里面有关于设置界面的修改
    Signed-off-by: bess <776536184@qq.com>

--- DacrsUI/SetAppFee.cpp
+++ DacrsUI/SetAppFee.cpp
@@ -24,12 +24,33 @@ CSetAppFee::~CSetAppFee()
 void CSetAppFee::DoDataExchange(CDataExchange* pDX)
 {
 	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_STATIC_LEAD , m_strTrading);
+	DDX_Control(pDX, IDC_STATIC_SB , m_strTrading2);
+	DDX_Control(pDX, IDC_STATIC_AB , m_strTrading3);
+	DDX_Control(pDX, IDC_STATIC_OB , m_strTrading4);
+	DDX_Control(pDX, IDC_STATIC_RLEAD , m_strTrading5);
+	DDX_Control(pDX, IDC_STATIC_SCR , m_strTx1);
+	DDX_Control(pDX, IDC_STATIC_GCR , m_strTx2);
+	DDX_Control(pDX, IDC_STATIC_SSR , m_strTx3);
+	DDX_Control(pDX, IDC_STATIC_ASR , m_strTx4);
+	DDX_Control(pDX, IDC_STATIC_AR , m_strTx5);
+	DDX_Control(pDX, IDC_STATIC_D , m_strDesTx1);
+	DDX_Control(pDX, IDC_STATIC_BETID , m_strDesTx2);
+	DDX_Control(pDX, IDC_STATIC_GRBID , m_strDesTx3);
+	DDX_Control(pDX, IDC_STATIC_IPOID , m_strDesTx4);
+
 }
 
 
 BEGIN_MESSAGE_MAP(CSetAppFee, CDialogEx)
 	ON_BN_CLICKED(IDOK, &CSetAppFee::OnBnClickedOk)
 	ON_BN_CLICKED(IDCANCEL, &CSetAppFee::OnBnClickedCancel)
+	ON_WM_ERASEBKGND()
+	ON_WM_CTLCOLOR()
+	ON_WM_SIZE()
+	ON_BN_CLICKED(IDC_BUTTON_CLOSE, &CSetAppFee::OnBnClickedButtonClose)
+	ON_WM_LBUTTONDOWN()
+	ON_WM_NCHITTEST()
 END_MESSAGE_MAP()
 
 
@@ -243,4 +264,125 @@ void CSetAppFee::OnBnClickedCancel()
 	// TODO: 在此添加控件通知处理程序代码
 
 	CDialogEx::OnCancel();
+}
+
+
+BOOL CSetAppFee::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// TODO:  在此添加额外的初始化
+	//SetBkBmpNid( IDB_BITMAP_DLG_BALCK ) ;
+	m_fontGrid.CreatePointFont(100,_T("新宋体"));
+	/// listbox 背景颜色
+	m_hbrush = CreateSolidBrush(RGB(255,236,229));
+	return TRUE;  // return TRUE unless you set the focus to a control
+	// 异常: OCX 属性页应返回 FALSE
+}
+
+
+BOOL CSetAppFee::OnEraseBkgnd(CDC* pDC)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	CRect   rect; 
+	GetClientRect(&rect); 
+
+	if(m_pBmp   !=   NULL) { 
+		BITMAP   bm; 
+		CDC   dcMem; 
+		::GetObject(m_pBmp,sizeof(BITMAP),   (LPVOID)&bm); 
+		dcMem.CreateCompatibleDC(NULL); 
+		HBITMAP     pOldBitmap   =(HBITMAP   )   dcMem.SelectObject(m_pBmp); 
+		pDC->StretchBlt(rect.left,rect.top-1,rect.Width(),rect.Height(),   &dcMem,   0,   0,bm.bmWidth-1,bm.bmHeight-1,   SRCCOPY); 
+
+		dcMem.SelectObject(pOldBitmap);
+		dcMem.DeleteDC();
+	} else  
+		CWnd::OnEraseBkgnd(pDC); 
+
+	return TRUE; 
+
+}
+void CSetAppFee::SetBkBmpNid( UINT nBitmapIn ) 
+{
+	if( NULL != m_pBmp ) {
+		::DeleteObject( m_pBmp ) ;
+		m_pBmp = NULL ;
+	}
+	m_pBmp	= NULL ;
+	HINSTANCE	hInstResource = NULL;	
+	hInstResource = AfxFindResourceHandle(MAKEINTRESOURCE(nBitmapIn), RT_BITMAP);
+	if( NULL != hInstResource ) {
+		m_pBmp = (HBITMAP)::LoadImage(hInstResource, MAKEINTRESOURCE(nBitmapIn), IMAGE_BITMAP, 0, 0, 0);
+	}
+}
+
+HBRUSH CSetAppFee::OnCtlColor(CDC* pDC, CWnd* pWnd, UINT nCtlColor)
+{
+	//HBRUSH hbr = CDialogEx::OnCtlColor(pDC, pWnd, nCtlColor);
+
+	//if (nCtlColor == CTLCOLOR_STATIC)
+	//{
+	//	pDC->SetBkMode(TRANSPARENT);
+	//	pDC->SelectObject(&m_fontGrid);
+	//	hbr = (HBRUSH)CreateSolidBrush(RGB(240,240,240));
+	//	return m_hbrush;
+	//}
+	//return hbr;
+
+	if (nCtlColor== CTLCOLOR_LISTBOX)
+	{
+		return m_hbrush; 
+	} 
+	else
+	{
+		return  CDialogEx::OnCtlColor(pDC, pWnd, nCtlColor);
+	}
+}
+
+
+void CSetAppFee::OnSize(UINT nType, int cx, int cy)
+{
+	CDialogEx::OnSize(nType, cx, cy);
+	if( NULL != GetSafeHwnd() ) {
+		const int div = 100 ;
+
+		CRect rc ;
+		GetClientRect( rc ) ;
+
+		CWnd *pst = GetDlgItem( IDC_BUTTON_CLOSE ) ;
+		if ( NULL != pst ) {
+			CRect rect ;
+			pst->GetClientRect( rect ) ;
+			pst->SetWindowPos( NULL ,rc.Width() - rect.Width() ,5  , rect.Width(), rect.Height()  ,SWP_SHOWWINDOW ) ; 
+		}
+	}
+	// TODO: 在此处添加消息处理程序代码
+}
+
+
+void CSetAppFee::OnBnClickedButtonClose()
+{
+	// TODO: 在此添加控件通知处理程序代码
+		EndDialog(IDOK);
+}
+
+
+void CSetAppFee::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+
+	SendMessage(WM_SYSCOMMAND, SC_MOVE, 0);//发送一个移动的消息
+	CDialogEx::OnLButtonDown(nFlags, point);
+}
+
+
+LRESULT CSetAppFee::OnNcHitTest(CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+
+	UINT nResult = CDialog::OnNcHitTest(point);
+	return nResult  == HTCLIENT ? HTCAPTION : nResult;//鼠标的坐标在客户区么？在的话就把它当成标题栏
+
+	return CDialogEx::OnNcHitTest(point);
 }
