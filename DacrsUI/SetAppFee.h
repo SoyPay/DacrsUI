b7d73e146d4bffa17214acbc8d0acd9acb69b976
bess <776536184@qq.com>
2015-07-01 19:10:25 +0800

    1.添加了菜单设置里面有关于设置界面的修改
    Signed-off-by: bess <776536184@qq.com>
    
    Former-commit-id: 25f5200c618bcf71be32002c1c6f3562d9ffded4

--- DacrsUI/SetAppFee.h
+++ DacrsUI/SetAppFee.h
@@ -1,5 +1,8 @@
 #pragma once
-
+#include "RoundButton.h"
+#include "StaticTrans.h"
+#include "BmpBtn.h"
+#include "ShadeButtonST.h"
 
 // CSetAppFee 对话框
 
@@ -23,4 +26,36 @@ public:
 	afx_msg void OnBnClickedCancel();
 	bool IsAllDigtal(const char* pData);
 	bool IsRegId(const char* pData);
+public:
+	CFont            m_fontGrid;//字体变量
+	HBITMAP		     m_pBmp; 
+	void		     SetBkBmpNid( UINT nBitmapIn ) ;
+	virtual BOOL OnInitDialog();
+	afx_msg BOOL OnEraseBkgnd(CDC* pDC);
+	afx_msg HBRUSH OnCtlColor(CDC* pDC, CWnd* pWnd, UINT nCtlColor);
+	afx_msg void OnSize(UINT nType, int cx, int cy);
+public:
+	CShadeButtonST   m_rBtnClose;
+	afx_msg void OnBnClickedButtonClose();
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg LRESULT OnNcHitTest(CPoint point);
+public:
+	CStaticTrans     m_strTrading  ;   
+	CStaticTrans     m_strTrading2  ;   
+	CStaticTrans     m_strTrading3  ;   
+	CStaticTrans     m_strTrading4  ;   
+	CStaticTrans     m_strTrading5  ;   
+
+	CStaticTrans     m_strTx1  ;  
+	CStaticTrans     m_strTx2  ;  
+	CStaticTrans     m_strTx3  ;  
+	CStaticTrans     m_strTx4  ;  
+	CStaticTrans     m_strTx5  ;  
+
+	CStaticTrans     m_strDesTx1  ;  
+	CStaticTrans     m_strDesTx2  ;  
+	CStaticTrans     m_strDesTx3  ;  
+	CStaticTrans     m_strDesTx4  ;  
+	CStaticTrans     m_strDesTx5  ;  
+	HBRUSH            m_hbrush;
 };
